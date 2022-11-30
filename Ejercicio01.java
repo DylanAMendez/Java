e4a28f585e91159c70b16679c17f61dd2c0d831d
Dylan <100449253+DylanAMendez@users.noreply.github.com>
2022-11-30 16:34:55 -0300

    Add files via upload

---
+++ Ejercicio01.java
@@ -0,0 +1,35 @@
+public class Ejercicio01 {
+
+
+    public static void main(String[] args) {
+
+        // numericos
+       byte numero1 = 5;
+       short numero2 = 7;
+       int numero3 = 9;
+       long numero4 = 11;
+
+       float numero5 = 7.7f;
+       double numero6 = 9.9d;
+
+       // boolean
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        // string
+        String string1 = "hello";
+        String string2 = "world";
+
+        // char - Caracteres
+        char char1 = 'a';
+
+        System.out.println("Numeros:" + " " + numero1 + " " + numero2 + " " + numero3 + " " + numero4 + " " + numero5 + " " + numero6);
+
+        System.out.println(bool1 + " " + bool2);
+
+        System.out.println(string1 + " " + string2);
+
+        System.out.println(char1);
+
+    }
+}
